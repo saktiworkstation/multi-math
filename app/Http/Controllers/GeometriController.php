552a3af45222e5d87ea0311213fe47f81f7a43bf
saktiworkstation <saktiworkstation@gmail.com>
2024-08-28 08:32:46 +0700

    Geometri : menambahkan beberapa fungsi geometri

--- app/Http/Controllers/GeometriController.php
+++ app/Http/Controllers/GeometriController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class GeometriController extends Controller
 {
-    //
+    public function panjangGaris($x1, $y1, $x2, $y2)
+    {
+        return sqrt(pow($x2 - $x1, 2) + pow($y2 - $y1, 2));
+    }
+
+    public function jarakTitikGaris($a, $b, $c, $x1, $y1)
+    {
+        return abs($a * $x1 + $b * $y1 + $c) / sqrt(pow($a, 2) + pow($b, 2));
+    }
+
+    public function luasSegitiga($a, $h)
+    {
+        return 0.5 * $a * $h;
+    }
+
+    public function luasLingkaran($r)
+    {
+        return pi() * pow($r, 2);
+    }
+
+    public function kelilingLingkaran($r)
+    {
+        return 2 * pi() * $r;
+    }
 }
