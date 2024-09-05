6dd8f66ec818a1fcd9d814dc28869a135f69ac2f
saktiworkstation <saktiworkstation@gmail.com>
2024-09-05 19:50:53 +0700

    fixed form

--- resources/views/geometri/keliling-lingkaran.blade.php
+++ resources/views/geometri/keliling-lingkaran.blade.php
@@ -3,18 +3,9 @@
 @section('container')
     <form>
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <label for="r" class="form-label">Nilai R</label>
+            <input type="number" class="form-control" id="r" name="r">
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Hitung</button>
     </form>
 @endsection
